89c23942f3b25a0fc044be442376bfd18f54e75b
AkhteryakovAV <akhteryakov@mail.ru>
2022-11-26 15:10:36 +0400

    Add project files.

---
+++ OpenTKLearning/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using OpenTKLearning.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using OpenTK.Windowing.Desktop;
+
+namespace OpenTKLearning
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            using (Game game = new Game(800, 600, "LearnOpenTK"))
+            {
+                game.Run();
+            }
+        }
+    }
+}
---
+++ OpenTKLearning/Views/Game.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace OpenTKLearning.Views
+{
+    public class Game : GameWindow
+    {
+        public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
+            : base(gameWindowSettings, nativeWindowSettings)
+        {
+        }
+        public Game(int width, int height, string title)
+            : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title })
+        {
+        }
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+            KeyboardState input = KeyboardState;
+
+            if (input.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
+    }
+}
